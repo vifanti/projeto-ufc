927c7e67607c7e450f66c5ce4a295cd1243f570d
Vinicius Fantinatto <vifanti@gmail.com>
2019-05-30 11:47:16 -0300

    teste http

--- ufcServer/lib/server.ts
+++ ufcServer/lib/server.ts
@@ -1,13 +1,16 @@
 import app from './app';
 import * as https from 'https';
+import * as http from 'http';
 import * as fs from 'fs';
 const PORT = 3000;
 
-const httpsOptions = {
-    key: fs.readFileSync('../config/key.pem'),
-    cert: fs.readFileSync('../config/cert.pem')
-}
+// const httpsOptions = {
+//     key: fs.readFileSync('../config/key.pem'),
+//     cert: fs.readFileSync('../config/cert.pem')
+// }
 
-https.createServer(httpsOptions, app).listen(PORT, () => {
+// https.createServer(httpsOptions, app).listen(PORT, () => {
+http.createServer(app).listen(PORT, () => {
+
     console.log('Express server listening on port ' + PORT);
 })
